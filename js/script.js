3b1b00f815c4dd010a8af362f65b19d81ea3a6c6
DavidWallJ <djkindread@gmail.com>
2018-02-20 18:03:52 +0800

    added carousel to team section

--- js/script.js
+++ js/script.js
@@ -6,3 +6,21 @@ $(window).on('load', function() {
 		.delay(300)
 		.fadeOut('slow');
 });
+
+// Team
+
+$(function() {
+	$('.owl-carousel').owlCarousel({
+		items: 2,
+		autoplay: true,
+		smartSpeed: 700,
+		loop: true,
+		autoplayHoverPause: true,
+		nav: true,
+		dots: false,
+		navText: [
+			'<i class="fa fa-angle-left"></i>',
+			'<i class="fa fa-angle-right"></i>'
+		]
+	});
+});
